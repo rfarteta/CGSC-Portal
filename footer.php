fc912761b1c4538759b7881c7c16251aa1722853
Rodger Arteta <rfarteta@gmail.com>
2018-12-20 11:10:22 +0800

    first commit

--- footer.php
+++
@@ -1,8 +0,0 @@
-<footer class="text-muted">
-      <div class="container">
-        <p class="float-right">
-          <a href="#">Back to top</a>
-        </p>
-        <p>All Rights Reserved © Academic and Registration Information System - Group 7 - AICS</p>
-      </div>
-</footer>
--- new.php
+++
@@ -1,3 +0,0 @@
-<form id="myform">
-  <input type="button" value="X" name="B1" onClick="parent.emailwindow.hide()" /></p>
-</form>
